2d6f126675985c0a0edd3132ed99c584a2641ccc
vasanthlmsace <vasanthr.lmsace@gmail.com>
2025-03-19 16:32:08 +0530

    Update the source.

--- classes/output/import_courselibrary_search.php
+++ classes/output/import_courselibrary_search.php
@@ -273,7 +273,7 @@ class import_courselibrary_search {
         }
         $modulesql = implode(" UNION ALL ", $moduleunions);
 
-        $select = " SELECT DISTINCT c.id, c.fullname, c.shortname, c.visible, c.sortorder ";
+        $select = " SELECT DISTINCT c.id, c.fullname, c.shortname, c.visible, c.sortorder, COALESCE(ul.timeaccess, 0) AS timeaccess ";
         $from   = " FROM {course} c
                     LEFT JOIN {user_lastaccess} ul ON ul.courseid = c.id AND ul.userid = :currentuser
                     LEFT JOIN {tag_instance} ti ON ti.itemid = c.id
@@ -319,7 +319,7 @@ class import_courselibrary_search {
                 $orderby = " ORDER BY c.fullname ASC";
                 break;
             case 'lastaccessed':
-                $orderby = " ORDER BY ul.timeaccess DESC";
+                $orderby = " ORDER BY timeaccess DESC";
                 break;
         }
 
